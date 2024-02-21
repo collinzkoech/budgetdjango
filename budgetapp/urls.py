8d2a8ff8c6440375f9af1293456054bb3ba80332
collinzkoech <collinskoech024@gmail.com>
2024-02-21 11:20:36 +0300

    initial commit

---
+++ budgetapp/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from budgetapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home),
+    path('about/',views.about),
+    path('form/',views.form),
+]
